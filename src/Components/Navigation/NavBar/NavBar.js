d5e2edbe27c36a309a35216390f65dd47f021cfd
anandamith <96160297+AnandBiniya@users.noreply.github.com>
2023-03-07 21:26:48 +0530

    toggle button commented

--- src/Components/Navigation/NavBar/NavBar.js
+++ src/Components/Navigation/NavBar/NavBar.js
@@ -71,7 +71,7 @@ const NavBar = () => {
                         socialClass={"navSocial"}
                         socialIcons={"socialIcons"}
                     />
-                    <ThemeToggle />
+                    {/* <ThemeToggle /> */}
                 </ul>
             </nav>
             <MobileNav
